c41c8334968f4fba81a34d341084870ce0f679cc
Frank <void@mailbox.org>
2023-02-02 23:21:15 +0100

    Check port availability

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,21 @@ const PT8005_BAUD: u32 = 9600;
 const PT8005_INDICATOR_DATA: u8 = 0x0D;
 const PT8005_TIMEOUT: u64 = 6000;
 
-fn _list_available_ports(){
+fn list_available_ports() {
     let ports = serialport::available_ports().expect("No ports found!");
     for p in ports {
-        println!("{}", p.port_name);
+        println!("Port {} is available.", p.port_name);
     }
+}
+
+fn is_port_available(port: String) -> bool {
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        if p.port_name == port {
+            return true;
+        }
+    }
+    false
 }
 
 fn main() {
@@ -19,21 +29,31 @@ fn main() {
 
     // let port_peaktech = "/dev/tty.usbserial-0001";
     let port_peaktech = "COM3";
+
+    if !(is_port_available(port_peaktech.to_string())) {
+        list_available_ports();
+        panic!("Port '{port_peaktech}' not available.");
+    }
+
     let mut port = serialport::new(port_peaktech, PT8005_BAUD)
         .timeout(Duration::from_millis(PT8005_TIMEOUT))
-        .open().expect("Failed to open port");
+        .open()
+        .expect("Failed to open port");
 
     loop {
         // read byte from serial port
         let mut serial_buf: Vec<u8> = vec![0; 1];
-        port.read(serial_buf.as_mut_slice()).expect("Found no data!");
+        port.read(serial_buf.as_mut_slice())
+            .expect("Found no data!");
         // I do not wait for a sync-byte, I just want to read dBA/dBC data :)
         // check whether data indicator byte was found then read two more bytes
         if serial_buf[0] == PT8005_INDICATOR_DATA {
             let mut serial_buf_b1: Vec<u8> = vec![0; 1];
-            port.read(serial_buf_b1.as_mut_slice()).expect("Found no data!");
+            port.read(serial_buf_b1.as_mut_slice())
+                .expect("Found no data!");
             let mut serial_buf_b2: Vec<u8> = vec![0; 1];
-            port.read(serial_buf_b2.as_mut_slice()).expect("Found no data!");
+            port.read(serial_buf_b2.as_mut_slice())
+                .expect("Found no data!");
             // byte1 to float
             let value1: f32 = (serial_buf_b1[0] * 10).into();
             // byte 2 - extract bit 5-8
@@ -42,7 +62,7 @@ fn main() {
             let value3: f32 = (serial_buf_b2[0] & 15).into();
             // assemble output from parts
             let output: f32 = value1 + value2 + (value3 / 10.0);
-            println!("Output {:?}", output );
+            println!("Output {:?}", output);
         }
     }
 }
